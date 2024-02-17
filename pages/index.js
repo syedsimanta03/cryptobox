843d0539e50b222341d9b1eb18acca37d57e1d92
syedsimanta03 <syed.simanta10@gmail.com>
2024-02-17 19:54:17 +0600

    bluebox

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import { Inter } from 'next/font/google'
 import GreenBox from '@/components/greenBox'
 import BlueBox from '@/components/BlueBox'
-import Transaction from '@/components/Transaction'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className={`md:px-16 md:py-16 px-8 py-8 ${inter.className}`}>
         <BlueBox />
-        <Transaction />
+        
     </main>
   )
 }
